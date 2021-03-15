07df08bc07d6550d63b5a05af564a55262b82551
TrungHieu <toletrunghieu@gmail.com>
2021-03-15 16:48:13 +0700

    addFile

---
+++ src/components/helpers/Db.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Db extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/components/pages/Singup.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Singup extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
